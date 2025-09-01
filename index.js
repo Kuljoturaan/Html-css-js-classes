d8a8fccf567ace5b6c807da8411eccbe45c3e0af
Kuljot Singh <kuljotsingh0511@gmail.com>
2025-09-01 18:50:11 +0530

    Error solve

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("Hello world!")
+// "use strict";
+console.log(6);
+
+// let height = 1000 
+// console.log(height);
+
+// let hei = 1000 
+// console.log(height);
+
+
+var length = 156;
+console.log(length)
+
+var length = 201;
+console.log(length)
+length = 2000
+console.log(length)
+
+let height = 240
+let anotherHeight= height
+console.log(height)
+console.log("height")
+console.log("height:"+height)
+console.log(anotherHeight)
+
+let weight 
+weight=75
+console.log(weight)
+
+count = 10
+console.log(count)
+
+weight = "100";
+console.log(weight)
+
+weight = weight + count
+console.log(weight)
+console.log(count + weight)
+
+const name = "kuljot singh"
+console.log("the name is ",name);
+//  name = "Sandeep"
+// console.log("the name is ",name);
+
+const age = 15 
+console.log("Age is",age)
