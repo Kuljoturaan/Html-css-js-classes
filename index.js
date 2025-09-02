b552c16b6a158924ce9dbdae23e02ea799a5bc39
Kuljot Singh <kuljotsingh0511@gmail.com>
2025-09-02 19:58:05 +0530

    Data type start

--- index.js
+++ index.js
@@ -1,46 +1,128 @@
-// "use strict";
-console.log(6);
+// // "use strict";
+// console.log(6);
 
-// let height = 1000 
-// console.log(height);
+// // let height = 1000 
+// // console.log(height);
 
-// let hei = 1000 
-// console.log(height);
+// // let hei = 1000 
+// // console.log(height);
 
 
-var length = 156;
-console.log(length)
+// var length = 156;
+// console.log(length)
 
-var length = 201;
-console.log(length)
-length = 2000
-console.log(length)
+// var length = 201;
+// console.log(length)
+// length = 2000
+// console.log(length)
 
-let height = 240
-let anotherHeight= height
-console.log(height)
-console.log("height")
-console.log("height:"+height)
-console.log(anotherHeight)
+// let height = 240
+// let anotherHeight= height
+// console.log(height)
+// console.log("height")
+// console.log("height:"+height)
+// console.log(anotherHeight)
 
-let weight 
-weight=75
-console.log(weight)
+// let weight 
+// weight=75
+// console.log(weight)
 
-count = 10
-console.log(count)
+// count = 10
+// console.log(count)
 
-weight = "100";
-console.log(weight)
+// weight = "100";
+// console.log(weight)
 
-weight = weight + count
-console.log(weight)
-console.log(count + weight)
+// weight = weight + count
+// console.log(weight)
+// console.log(count + weight)
 
-const name = "kuljot singh"
-console.log("the name is ",name);
-//  name = "Sandeep"
+// const name = "kuljot singh"
 // console.log("the name is ",name);
+// //  name = "Sandeep"
+// // console.log("the name is ",name);
 
-const age = 15 
-console.log("Age is",age)
+// const age = 15 
+// console.log("Age is",age)
+// {
+//     counter = 2;
+//     console.log(counter)
+    
+//     let count = 2
+//     console.log("count:",count)
+// }
+//  count = 1
+//  console.log("count:",count)
+//  counter= counter + 1
+//  console.log(counter)
+
+
+//  let count1 = 5
+//  console.log("count define me " ,count1)
+
+// { var counter = 10
+//  console.log("counter define me ",counter)
+// }
+//  console.log(counter)
+
+// let height = 180
+// {
+//     let weight = 70
+//     console.log(weight)
+//     console.log(height)
+// }
+//  console.log(height)
+//  console.log(weight)
+
+
+// function helloWorldFirst () {
+//     console.log("Hello")
+//     console.log("World")
+// }
+// helloWorldFirst()
+// console.log("try it again")
+// helloWorldFirst();
+// console.log("try again")
+// helloWorldFirst()
+
+// let counter = 100
+// console.log("Count:",counter)
+// {
+//     let counter = 10
+//     console.log("Count:",counter)
+// }
+// console.log("Count:",counter)
+// console.log("counter is the",counter);
+// var counter =100
+// function printCounter () {
+//     let counter = 200
+//     console.log("the counter is ",counter)
+// }
+// console.log("the counter is ",counter)
+// printCounter();
+// console.log("the counter is ",counter)
+
+// var counter =100;
+// console.log("Counter is ",counter)
+// printConter()
+// function printCounter () {
+// //     let counter = 200
+// //     console.log("the counter is ",counter)
+// // }
+
+let year = 1990
+console.log("The year is:",year)
+console.log("The type of year is:",typeof year)
+
+let month 
+console.log("the month is :",month)
+console.log("The typeof month is:",typeof month)
+
+let name = "Kuljot"
+console.log("the name is :",name)
+console.log("the typeof name is :",typeof name)
+
+let typeOfName = typeof name
+
+console.log("the name is :",name)
+console.log("typeOfName: ",typeOfName)
