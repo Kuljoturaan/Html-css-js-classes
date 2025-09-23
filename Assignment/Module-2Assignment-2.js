828e0363c975b79cd3f7901b2b33733d1582697e
Kuljot Singh <kuljotsingh0511@gmail.com>
2025-09-23 11:05:33 +0530

    Module-2Assignment-2 complete

--- Assignment/Module-2Assignment-2.js
+++ Assignment/Module-2Assignment-2.js
@@ -1,21 +1,19 @@
 const  Rose = 8
-const Lilly = 10
+const  Lilly = 10
 const  Tulip = 2
 
 let roseQuantity = 70
 let lillyQuantity = 50
 let tulipQuantity = 120 
 
-let decreaseRoses = roseQuantity -  20
-let decreaseLilly = lillyQuantity - 30
 
-let roseValue = Rose * roseQuantity
-let lillyValue = Lilly * lillyQuantity
-let tulipValue = Tulip * tulipQuantity
 
-let total = roseValue + lillyValue + tulipValue;
+console.log("Rose price :", Rose, "Rose quantity is :", roseQuantity)
+console.log("Lilly price :", Lilly, "lilly Quantity is :", lillyQuantity)
+console.log("Tulip price :", Tulip, "tulip quantity is :", tulipQuantity)
 
-console.log("Rose price :", Rose, "Rose quantity is :", roseQuantity, "The rose value is :", roseValue)
-console.log("Lilly price :", Lilly, "lilly Quantity is :", lillyQuantity, "The lilly value is :", lillyValue)
-console.log("Tulip price :", Tulip, "tulip quantity is :", tulipQuantity, "The tulip value is :", tulipValue)
-console.log(total)
+roseQuantity -= 20
+lillyQuantity -= 30 
+console.log("Rose price :", Rose, "Rose quantity is :", roseQuantity)
+console.log("Lilly price :", Lilly, "lilly Quantity is :", lillyQuantity)
+console.log("Tulip price :", Tulip, "tulip quantity is :", tulipQuantity)
