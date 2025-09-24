d16baca8d8da1ec76074f3b8fd709b430126150a
Kuljot Singh <kuljotsingh0511@gmail.com>
2025-09-24 19:10:42 +0530

    Module-3 complete user interaction

---
+++ Assignment/UserInteraction.js
@@ -0,0 +1,14 @@
+ console.log("this is first line of js ")
+
+        function myfunction () {
+            console.log("Hello world is clicked and country India")
+        }
+        function confirmDialog () {
+            let decision = window.confirm("It is ok again")
+            console.log(decision)
+        } 
+        function comfirmDialog1 () {
+            let remove = confirm("Remove all data")
+            let message = remove ? "Deleted data": "cancelled"
+            console.log(message)
+        }
