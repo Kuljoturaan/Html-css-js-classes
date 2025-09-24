224e7ee3e6c5bf9e60c0dfb3ab4bce127d159d90
Kuljot Singh <kuljotsingh0511@gmail.com>
2025-09-24 20:32:08 +0530

    Assignment-5 complete

---
+++ Assignment/Assignment-5.js
@@ -0,0 +1,20 @@
+    // 1
+let ticket = {
+    From : "Sehore",
+    To :"Bhopal",
+    price: 200
+}
+console.log("====Ticket Details====")
+console.log("From",ticket.From)
+console.log("To",ticket.To)
+console.log("Price",ticket.price)
+
+//   2
+let person = {}
+
+person.firstName = "Kuljot"
+person.middleName = "Singh"
+person.lastName = "Rajpal"
+console.log("firstName",person.firstName)
+console.log("middleName",person.middleName)
+console.log("lastName",person.lastName)
