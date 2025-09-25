2acbf66e7faddf717b1ac138e757666badf94c27
Kuljot Singh <kuljotsingh0511@gmail.com>
2025-09-25 20:02:32 +0530

    Module-4 start if else condition

--- Assignment/Module-2-Assignment-1.js
+++ Assignment/Module-2-Assignment-1.js
@@ -1,18 +1,18 @@
-let Rose = 8
-let Lilly = 10
-let Tulip = 2
+// let Rose = 8
+// let Lilly = 10
+// let Tulip = 2
 
-let roseQuantity = 70
-let lillyQuantity = 50
-let tulipQuantity = 120 
+// let roseQuantity = 70
+// let lillyQuantity = 50
+// let tulipQuantity = 120 
 
-let roseValue = Rose * roseQuantity
-let lillyValue = Lilly * lillyQuantity
-let tulipValue = Tulip * tulipQuantity
+// let roseValue = Rose * roseQuantity
+// let lillyValue = Lilly * lillyQuantity
+// let tulipValue = Tulip * tulipQuantity
 
-let total = roseValue + lillyValue + tulipValue;
+// let total = roseValue + lillyValue + tulipValue;
 
-console.log("Rose price :", Rose, "Rose quantity is :", roseQuantity, "The rose value is :", roseValue)
-console.log("Lilly price :", Lilly, "lilly Quantity is :", lillyQuantity, "The lilly value is :", lillyValue)
-console.log("Tulip price :", Tulip, "tulip quantity is :", tulipQuantity, "The tulip value is :", tulipValue)
-console.log(total)
+// console.log("Rose price :", Rose, "Rose quantity is :", roseQuantity, "The rose value is :", roseValue)
+// console.log("Lilly price :", Lilly, "lilly Quantity is :", lillyQuantity, "The lilly value is :", lillyValue)
+// console.log("Tulip price :", Tulip, "tulip quantity is :", tulipQuantity, "The tulip value is :", tulipValue)
+// console.log(total)
--- Assignment/Module-2Assignment-2.js
+++ Assignment/Module-2Assignment-2.js
@@ -1,19 +1,19 @@
-const  Rose = 8
-const  Lilly = 10
-const  Tulip = 2
+// const  Rose = 8
+// const  Lilly = 10
+// const  Tulip = 2
 
-let roseQuantity = 70
-let lillyQuantity = 50
-let tulipQuantity = 120 
+// let roseQuantity = 70
+// let lillyQuantity = 50
+// let tulipQuantity = 120 
 
 
 
-console.log("Rose price :", Rose, "Rose quantity is :", roseQuantity)
-console.log("Lilly price :", Lilly, "lilly Quantity is :", lillyQuantity)
-console.log("Tulip price :", Tulip, "tulip quantity is :", tulipQuantity)
+// console.log("Rose price :", Rose, "Rose quantity is :", roseQuantity)
+// console.log("Lilly price :", Lilly, "lilly Quantity is :", lillyQuantity)
+// console.log("Tulip price :", Tulip, "tulip quantity is :", tulipQuantity)
 
-roseQuantity -= 20
-lillyQuantity -= 30 
-console.log("Rose price :", Rose, "Rose quantity is :", roseQuantity)
-console.log("Lilly price :", Lilly, "lilly Quantity is :", lillyQuantity)
-console.log("Tulip price :", Tulip, "tulip quantity is :", tulipQuantity)
+// roseQuantity -= 20
+// lillyQuantity -= 30 
+// console.log("Rose price :", Rose, "Rose quantity is :", roseQuantity)
+// console.log("Lilly price :", Lilly, "lilly Quantity is :", lillyQuantity)
+// console.log("Tulip price :", Tulip, "tulip quantity is :", tulipQuantity)
