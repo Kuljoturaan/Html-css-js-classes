7aeaf3f3acc1caeb764896c85b56808c429bc052
Kuljot Singh <kuljotsingh0511@gmail.com>
2025-12-25 21:52:26 +0530

    Assignment -10 task 2nd completed

--- Assignment/Assignment-10.js
+++ Assignment/Assignment-10.js
@@ -1,9 +1,16 @@
-let count = 0 
+// let count = 0
 
-let enterValid = setInterval(() => {
-    console.log(count)
-    count++
-    
-    if(count > 10) 
-        clearInterval(enterValid)
-},2000)
+// let enterValid = setInterval(() => {
+//     console.log(count)
+//     count++
+
+//     if (count > 10)
+//         clearInterval(enterValid)
+// }, 2000)
+
+function printNumber() {
+    for (let i = 0; i <= 10; i++) {
+        console.log(i)
+    }
+}
+printNumber()
